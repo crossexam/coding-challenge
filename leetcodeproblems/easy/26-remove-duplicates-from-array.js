4713c5753a188c60163895aea74824fdbac2ba80
Chau Giang <giangc@acm.org>
2020-10-23 11:15:25 +0000

    (leetcode, easy, js-cpp) add solution for remove duplicates from array

---
+++ leetcodeproblems/easy/26-remove-duplicates-from-array.js
@@ -0,0 +1,36 @@
+
+/*
+  Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+  Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+
+  Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+  Clarification:
+
+  Confused why the returned value is an integer but your answer is an array?
+
+  Note that the input array is passed in by reference, which means a modification to the input array will be known to the caller as well.
+
+  Internally you can think of this:
+
+  // nums is passed in by reference. (i.e., without making a copy)
+  int len = removeDuplicates(nums);
+
+  // any modification to nums in your function would be known by the caller.
+  // using the length returned by your function, it prints the first len elements.
+  for (int i = 0; i < len; i++) {
+      print(nums[i]);
+  }
+*/
+
+var removeDuplicates = function(nums) {
+ if(nums.length < 1) return 0; 
+ let i = 0;
+ for (j = i + 1; j < nums.length; j++) {
+   if(nums[i]!==nums[j]) {
+     i++;
+     nums[i] = nums[j];
+   }
+ }
+  return i+1;
+};
